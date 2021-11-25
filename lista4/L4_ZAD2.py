307cc178eea46a7ad9070b05ee67fa74aa1d7e57
Julia Mazur <mazurka0609@gmail.com>
2021-11-25 15:33:35 +0100

    first draft 1,2,4

---
+++ lista4/L4_ZAD2.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python 
+"""
+Zaprojektuj i przeprowadź eksperyment porównujący wydajność implementacji kolejek QueueBaB i QueueBaE.
+"""
+import time
+from matplotlib import pyplot as plt
+from L4_ZAD1 import QueueBaB
+from L4_ZAD1 import QueueBaE
+
+if __name__ == "__main__":
+
+    xaxis = range(100,1000,5)
+    BaB = QueueBaB()
+    BaE = QueueBaE()
+    
+    #--------------adding to the queue------------------------
+    on_list_time1 = []
+    on_list_time2 = []
+
+    for i in xaxis:
+        start = time.time()
+        for j in range(i):
+            BaB.enqueue(j)
+        end = time.time()
+        on_list_time1.append(end-start)
+
+    for i in xaxis:
+        start = time.time()
+        for j in range(i):
+            BaE.enqueue(j)
+        end = time.time()
+        on_list_time2.append(end-start)
+    
+    plt.scatter(xaxis,on_list_time1, label = "BaB")
+    plt.scatter(xaxis,on_list_time2, label = "BaE")
+    plt.title("Adding to the queue")
+    plt.legend()
+    plt.show()
+
+    #--------------removing from the queue---------------------
+    of_list_time1 = []
+    of_list_time2 = []
+
+    for i in xaxis:
+        start = time.time()
+        for j in range(i):
+            BaB.dequeue()
+        end = time.time()
+        of_list_time1.append(end-start)
+
+    for i in xaxis:
+        start = time.time()
+        for j in range(i):
+            BaE.dequeue()
+        end = time.time()
+        of_list_time2.append(end-start)
+
+    plt.scatter(xaxis,of_list_time1, label = "BaB")
+    plt.scatter(xaxis,of_list_time2, label = "BaE")
+    plt.title("removing from the queue")
+    plt.legend()
+    plt.show()
+
+    #-----------adding and removing the queue------------------
+
+    list_time1 = []
+    list_time2 = []
+
+    for i in xaxis:
+        start = time.time()
+        for j in range(i):
+            BaB.enqueue(j)
+        for j in range(i):
+            BaB.dequeue()
+        end = time.time()
+        list_time1.append(end-start)
+    
+    for i in xaxis:
+        start = time.time()
+        for j in range(i):
+            BaE.enqueue(j)
+        for j in range(i):
+            BaE.dequeue()
+        end = time.time()
+        list_time2.append(end-start)
+    
+    plt.scatter(xaxis,list_time1, label = "BaB")
+    plt.scatter(xaxis,list_time2, label = "BaE")
+    plt.title("adding and removing from the queue")
+    plt.legend()
+    plt.show()
+
+    #-------------difference between BaB and BaE------------------
+
+    plt.scatter(xaxis,[list_time1[i]-list_time2[i] for i in range(len(list_time1))], label = "BaB")
+    plt.title("comparison (BaB time - BaE time)")
+    plt.show()
+
+    # maybe add some functions???
---
+++ lista4/L4_ZAD4.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python 
+
+class stack():
+
+    def __init__(self):
+        self.list_of_items = []
+    
+    def push(self, item):
+        self.list_of_items.append(item)
+    
+    def pop(self):
+        return self.list_of_items.pop()
+    
+    def peek(self):
+        return self.list_of_items[len(self.list_of_items)-1]
+    
+    def isEmpty(self):
+        return self.list_of_items == []
+    
+    def size(self):
+        return len(self.list_of_items)
+
+
+def checking_HTML_correctness(filename):
+    """
+    Funkcja ma za zadanie sprawdzać poprawność składni dokumentu HTML.
+    Jako argument przyjmuje nazwę pliku, który ma sprawdzić.
+    Zwraca True jeśli dokument jest poprawny składniowo i False jeśli nie jest.
+    """
+    file_obj = open(filename, 'r')
+    text = file_obj.read()
+    single = ['!DOCTYPE', 'meta', 'link', 'img', 'br', 'hr']
+    indicators = stack()
+
+    text_comments = str(text).split('-->')
+    for i in range(len(text_comments)-1):
+        if text_comments[i].count('<!--') != 1:
+            return False
+        else:
+            index = text_comments[i].find('<!--')
+            text_comments[i] = text_comments[i][:index]
+    text_no_comments = ''.join(text_comments)
+
+    
+    text = str(text_no_comments).split('>')[:-1]
+    for i in text:
+        if i.count('<') != 1:
+            return False
+    for i in text:
+        position = i.find('<')
+        if position+1<len(i):
+            if i[position+1] == '/':
+                if position+2<len(i):
+                    if ' ' in i[position+1:]:
+                        end = i[position+1:].find(' ')
+                        word = i[position+1:position+end+1]
+                        if not (word in single):
+                            if indicators.isEmpty():
+                                return False
+                    else:
+                        word = i[position+1:]
+                    
+                    if not (word in single):
+                        if not (word == '/' + indicators.peek()):
+                            return False
+                        else:
+                            indicators.pop()
+                else:
+                    return False
+            else:
+                if ' ' in i[position+1:]:
+                    end = i[position+1:].find(' ')
+                    word = i[position+1:position+end+1]
+                else:
+                    word = i[position+1:]
+                if not (word in single):
+                    indicators.push(word)
+        else:
+            return False
+    
+    if indicators.isEmpty():
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    print(checking_HTML_correctness('L4_ZAD4_sampleHTML_1.txt'))
+    print(checking_HTML_correctness('L4_ZAD4_sampleHTML_2.txt'))
+    print(checking_HTML_correctness('L4_ZAD4_sampleHTML_3.txt'))
+    # " " at the beggining
