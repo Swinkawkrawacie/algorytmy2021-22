3f24d92f75011e89b938bf751562edc7e6eeedb3
Julia Mazur <mazurka0609@gmail.com>
2022-01-08 14:32:32 +0100

    +,-,* and I think /

--- lista6/4.py
+++ lista6/4.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 class parse_tree():
-    def __init__(self, text):
+    def __init__(self, text:str):
         operator_list = []
-        while text != '':
+        while len(text)>0:
             for j in ['sin', 'cos', 'exp']:
                 if text[:3] == j:
                     operator_list.append(j)
@@ -18,22 +18,104 @@ class parse_tree():
         self.data = None
         self.left = None
         self.right = None
+        self.leftover_text = operator_list
         self.build_tree()
 
     def build_tree(self):
 
-        for i in range(len(self.text)):
-            if self.text[i] == '(':
-                self.left = parse_tree(self.text[i+1:])
-            elif self.text[i] in ['+','-','*','/','^']:
-                self.data = self.text[i]
-                self.right = parse_tree(self.text[i+1:])
-            elif self.text[i] in ['sin', 'cos', 'exp', 'ln']:
-                self.data = self.text[i]
-                self.left = parse_tree(self.text[i+1:])
+        while len(self.leftover_text)>0:
+            if self.leftover_text[0] == '(':
+                self.left = parse_tree(self.leftover_text[1:])
+                self.leftover_text = self.left.leftover_text
+            elif self.leftover_text[0] in ['+','-','*','/','^']:
+                self.data = self.leftover_text[0]
+                self.right = parse_tree(self.leftover_text[1:])
+                self.leftover_text = self.right.leftover_text
+            elif self.leftover_text[0] in ['sin', 'cos', 'exp', 'ln']:
+                self.data = self.leftover_text[0]
+                self.left = parse_tree(self.leftover_text[1:])
                 self.right = None
-                #i need this current previous stuff
+                self.leftover_text = self.left.leftover_text
+            elif self.leftover_text[0] == ')':
+                self.leftover_text = self.leftover_text[1:]
+                return
+            else:
+                self.data = self.leftover_text[0]
+                self.leftover_text = self.leftover_text[1:]
+                return
+    
+    def __str__(self):
+        return 'N'+str(self.data) + '\n L' + str(self.left) + '\t R' + str(self.right)
 
+class diff_tree():
+    def __init__(self, base_tree:parse_tree):
+        self.base = base_tree
+        self.new_data = None
+        self.new_left = None
+        self.new_right = None
+
+    def diff(self):
+        current = self.base
+        if current.data in ['+', '-']:
+            self.new_data = current.data
+            left_child = diff_tree(current.left)
+            left_child.diff()
+            self.new_left = left_child
+            right_child = diff_tree(current.right)
+            right_child.diff()
+            self.new_right = right_child
+        elif current.data == '*':
+            self.new_data = '+'
+            self.new_left = diff_tree(current)
+            self.new_left.new_data = '*'
+            left_child = diff_tree(current.left)
+            left_child.diff()
+            self.new_left.new_left = left_child
+            self.new_left.new_right = current.right
+            
+            self.new_right = diff_tree(current)
+            self.new_right.new_data = '*'
+            right_child = diff_tree(current.right)
+            right_child.diff()
+            self.new_right.new_right = right_child
+            self.new_right.new_left = current.left
+        elif current.data == '/':
+            self.new_data = current.data
+            self.new_left = diff_tree(current)
+
+            self.new_left.new_data = '-'
+            self.new_left.new_left = diff_tree(current)
+
+            self.new_left.new_left.new_data = '*'
+            left_child = diff_tree(current.left)
+            left_child.diff()
+            self.new_left.new_left.new_left = left_child
+            self.new_left.new_left.new_right = current.right
+
+            self.new_left.new_right = diff_tree(current)
+            self.new_left.new_right.new_data = '*'
+            self.new_left.new_right.new_left = current.left
+            right_child = diff_tree(current.right)
+            right_child.diff()
+            self.new_left.new_right.new_right = right_child
+
+            self.new_right = diff_tree(current)
+            self.new_right.new_data = '^'
+            self.new_right.new_left = current.right
+            self.new_right.new_right = '2'
+        elif current.data == 'x':
+            self.new_data = 1
+        else:
+            self.new_data = 0
+    
+    def __str__(self):
+        return 'N'+str(self.new_data) + '\n L' + str(self.new_left) + '\t R' + str(self.new_right)
 
 if __name__ == "__main__":
-    pass
+    tree = parse_tree('(((3+5)*4)*sin(x+1))')
+    print(tree)
+    print('----------------------------------------')
+    tree1 = parse_tree('((3+5)*4)')
+    diff = diff_tree(tree1)
+    diff.diff()
+    print(diff)
